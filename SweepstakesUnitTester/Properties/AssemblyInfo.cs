a3a37ec1990bf32cb87e41f22deb82e67a720f13
Forrest Morrisey <60801874+fmorrisey@users.noreply.github.com>
2020-10-31 13:51:05 -0500

    Created a uniqueID method that creates uniqueIDs for contestans key value. Also intitalized a Unit Testing project.

---
+++ SweepstakesUnitTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SweepstakesUnitTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SweepstakesUnitTester")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0039812a-1baf-4159-b553-048029ad2062")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
